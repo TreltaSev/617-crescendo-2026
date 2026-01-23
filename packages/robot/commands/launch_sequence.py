372ec28f0d7cd9ec05213360c8e49c8d2527a768
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2026-01-23 17:00:57 -0500

    feat(commands): Add launch sequence command

--- packages/robot/commands/launch_sequence.py
+++ packages/robot/commands/launch_sequence.py
@@ -1,4 +1,28 @@
-from commands2 import CommandScheduler
+from __future__ import annotations
 
-class LaunchSequence(CommandScheduler):
-    pass
+# === Types ===
+from commands2 import Command, SequentialCommandGroup
+
+from commands import SpinUp, Launch
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from subsystems import FuelSubsystem
+
+# === Constants ===
+from utils.constants.fuel_constants import (
+    SPIN_UP_SECONDS
+)
+
+
+class LaunchSequence(SequentialCommandGroup):
+    """
+    Creates a new LaunchSequence
+    """
+
+    def __init__(self, fuel_system: FuelSubsystem, *commands: Command):
+        self.addCommands(
+            SpinUp(fuel_system).withTimeout(SPIN_UP_SECONDS),
+            Launch(fuel_system),
+            *commands
+        )
