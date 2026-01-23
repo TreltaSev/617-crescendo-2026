7ee390ed8a3abd3d5c0163ad7cc6eb65a7256d41
Trelta Sebastion laptop:linux <treltasev@gmail.com>
2026-01-23 16:46:10 -0500

    feat(commands): Added example command group

--- packages/robot/commands/example_auto.py
+++ packages/robot/commands/example_auto.py
@@ -1,4 +1,23 @@
-from commands2 import SequentialCommandGroup
+from __future__ import annotations
+
+# === Types ===
+from commands2 import Command, SequentialCommandGroup
+
+from commands import AutoDrive, Launch
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from subsystems import DriveSubsystem, FuelSubsystem
+
 
 class ExampleAuto(SequentialCommandGroup):
-    pass
+    """
+    Example auto drive and launch command group
+    """
+
+    def __init__(self, drive_system: DriveSubsystem, fuel_system: FuelSubsystem, *commands: Command):
+        self.addCommands(
+            AutoDrive(drive_system, 0.5, 0.0).withTimeout(.25),
+            Launch(fuel_system),
+            *commands
+        )
